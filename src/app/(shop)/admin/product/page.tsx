23ffbfba493245f499649b1b934435c51686cec6
josuerangel2005 <josuerangel262005@gmail.com>
2026-08-22 16:28:52 -0500

    fix(admin): pass required categories prop to product form page

--- src/app/(shop)/admin/product/page.tsx
+++ src/app/(shop)/admin/product/page.tsx
@@ -1,11 +1,13 @@
 import { Title } from "../../../../../ui";
+import { getAllCategoriesAction } from "../../../../../ui/features/admin/product/actions/get-all-categories-action";
 import { ProductForm } from "../../../../../ui/features/admin/product/components/ProductForm";
 
-export default function () {
+export default async function () {
+  const categories = await getAllCategoriesAction();
   return (
     <>
       <Title title="Crear" subTitle="Guardar un nuevo producto" />
-      <ProductForm />{" "}
+      <ProductForm categories={categories} />{" "}
     </>
   );
 }
